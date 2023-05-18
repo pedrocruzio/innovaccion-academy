04001fc05279a4a9e51aa93e1fd0b29425b2c38e
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2023-05-17 22:32:46 -0400

    Pipeline has Quizzess

---
+++ src/constants/imageLinks.ts
@@ -0,0 +1,91 @@
+const IMAGELINKS = {
+    'Going Bankless': [
+        '/lesson/going-bankless/welcome-to-InnovAccion-cf9ea670.png',
+        '/lesson/going-bankless/upgrading-humanitys-coordination-protocols-2ef0d764.png',
+        '/lesson/going-bankless/your-bankless-journey-9736d072.png'
+    ],
+    'Wallet Basics': [
+            '/lesson/wallet-basics/wallet-intro-7b45d75e.png',
+            '/lesson/wallet-basics/wallet-definition-e8f8f9a8.svg',
+            '/lesson/wallet-basics/recovery-phrase-c2d8fa26.svg',
+            '/lesson/wallet-basics/public-key-b6387071.svg',
+            '/lesson/wallet-basics/private-key-ca271641.svg',
+            '/lesson/wallet-basics/custodial-wallet-29ed2b65.svg',
+            '/lesson/wallet-basics/non-custodial-wallet-aee2708b.svg',
+            '/lesson/wallet-basics/hot-wallet-1d444d7c.svg',
+            '/lesson/wallet-basics/cold-wallet-59646edb.svg',
+            '/lesson/wallet-basics/metamask-wallet-521de184.svg'
+        ],
+    'Blockchain Basics': [
+        '/lesson/blockchain-basics/introduction-6d0b6137.svg',
+        '/lesson/blockchain-basics/blockchain-structure-346dae14.svg',
+        '/lesson/blockchain-basics/examining-the-ledger-74e5f072.svg',
+        '/lesson/blockchain-basics/transactions-on-the-ledger-f4f9d470.svg',
+        '/lesson/blockchain-basics/block-anatomy-8ba3bea2.svg',
+        '/lesson/blockchain-basics/inside-a-block-b11c74ce.svg',
+        '/lesson/blockchain-basics/individual-transactions-2f6bf118.svg',
+        '/lesson/blockchain-basics/user-addresses-e9456d37.svg'
+    ],
+    'Web3 Security': [
+        '/lesson/web3-security/money-in-web2-78779f3c.svg',
+        '/lesson/web3-security/money-in-web3--832997fe.svg',
+        '/lesson/web3-security/two-factor-authentication-75e29563.svg',
+        '/lesson/web3-security/social-engineering-scams-5750ad84.svg',
+        '/lesson/web3-security/social-media-safety-fbe97b9e.svg',
+        '/lesson/web3-security/social-media-best-practices-0776e310.svg',
+        '/lesson/web3-security/scam-tokens-cbab390e.svg',
+        '/lesson/web3-security/hardware-wallets-191cacd1.svg',
+        '/lesson/web3-security/wallet-strategies-857fd09c.svg'
+    ],
+    'Layer 1 Blockchains': [
+        '/lesson/layer-1-blockchains/introduction-e0da8469.svg',
+        '/lesson/layer-1-blockchains/blockchain-trilemma-31329f66.svg',
+        '/lesson/layer-1-blockchains/security-and-consensus-5e1f03b6.svg',
+        '/lesson/layer-1-blockchains/security-and-attacks-7d35acf6.svg',
+        '/lesson/layer-1-blockchains/scalability-throughput-4b140ad0.svg',
+        '/lesson/layer-1-blockchains/scalability-finality-161f7e2b.svg',
+        '/lesson/layer-1-blockchains/decentralization-distributes-power-829f91a2.svg',
+        '/lesson/layer-1-blockchains/is-it-decentralized-4b5bf857.svg',
+        '/lesson/layer-1-blockchains/some-examples-523b9b96.svg',
+        '/lesson/layer-1-blockchains/so-what-can-be-done-50592b46.svg',
+        '/lesson/layer-1-blockchains/the-future-of-ethereum-2b64fd6b.svg'
+    ],
+    'Layer 2 Blockchains': [
+        '/lesson/layer-2-blockchains/introduction-0d584167.svg',
+        '/lesson/layer-2-blockchains/payment-channels-5e29dfd3.svg',
+        '/lesson/layer-2-blockchains/bridging-layer-1-and-layer-2-8268ff20.svg',
+        '/lesson/layer-2-blockchains/sidechains-406fe8d1.svg',
+        '/lesson/layer-2-blockchains/rollups-ea8672d0.svg',
+        '/lesson/layer-2-blockchains/optimistic-rollups-75b31999.svg',
+        '/lesson/layer-2-blockchains/zk-rollups-bc7c293e.svg',
+        '/lesson/layer-2-blockchains/cross-chain-dapp-compatibility-eeb2b414.svg',
+        '/lesson/layer-2-blockchains/start-your-layer-2-journey-with-optimism--7999722b.svg'
+    ],
+    'Intro to DeFi': [
+        '/lesson/intro-to-defi/defi-defined-46782447.svg',
+        '/lesson/intro-to-defi/earning-yield-c5f123f8.svg',
+        '/lesson/intro-to-defi/what-you-can-do-with-defi-9b17cf2e.svg',
+        '/lesson/intro-to-defi/investing-d99a6d1f.svg',
+        '/lesson/intro-to-defi/trading-8cd72977.svg',
+        '/lesson/intro-to-defi/lending-borrowing-4fb1c7c3.svg',
+        '/lesson/intro-to-defi/staking-b4b4319d.svg',
+        '/lesson/intro-to-defi/defi-downsides-51dd6225.svg',
+        '/lesson/intro-to-defi/defi-downsides-3a6ce496.svg',
+        '/lesson/intro-to-defi/defi-downsides-83a9391f.svg'
+    ],
+    'DEXs and AMMs': [],
+    'DEX Aggregators': [
+        '/lesson/dex-aggregators/introduction-ba453b68.svg',
+        '/lesson/dex-aggregators/an-example-of-how-liquidity-impacts-prices-915b3d84.svg',
+        '/lesson/dex-aggregators/shortcomings-of-traditional-dexs-thin-liquidity-c9e0b695.svg',
+        '/lesson/dex-aggregators/recombining-liquidity-with-dex-aggregators-f01777dd.svg',
+        '/lesson/dex-aggregators/how-gas-cost-is-calculated-on-ethereum-c34efe86.svg',
+        '/lesson/dex-aggregators/how-aggregators-reduce-gas-costs-for-users-28d6f207.svg',
+        '/lesson/dex-aggregators/meta-aggregators-100793fd.svg',
+        '/lesson/dex-aggregators/avoiding-sandwich-attacks-75f6ae82.svg',
+        '/lesson/dex-aggregators/more-protection-from-sandwiches-otc-trades-04ef66c9.svg'
+    ],
+    'Kudos testing': []
+}
+
+export default IMAGELINKS

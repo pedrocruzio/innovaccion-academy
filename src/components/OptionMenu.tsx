b0d8167a51791c7329897a15bb46391a75f6bd7a
pedrocruzio <pedro@rokailabs.com>
2023-07-25 08:45:09 -0400

    update link

--- src/components/OptionMenu.tsx
+++ src/components/OptionMenu.tsx
@@ -42,7 +42,7 @@ const OptionMenu = ({
           ...
         </MenuButton>
         <MenuList css={{ background: '#6064fc' }}>
-        <ExternalLink href="https://innovaccionpr.org" color="white">
+        <ExternalLink href="https://en.academy.innovaccionpr.org" color="white">
             <MenuItem>English</MenuItem>
           </ExternalLink>
           <ExternalLink href="https://innovaccionpr.org" color="white">
